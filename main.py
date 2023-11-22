5b135a2c4d220c3809165390a7299e6ebad922d6
sirrsina <143832198+sirrsina@users.noreply.github.com>
2023-11-22 19:06:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ def get_info(card_number:str):
 
 def enter():
     data = get_info(filedialog.askopenfilename())
-    print(data)
-
-    if e_pin.get()==data['pin']:
+    if data==None:
+        return
+    elif e_pin.get()==data['pin']:
         manage_window.deiconify()
         root.withdraw()
     else:
-        messagebox.showwarning("Warning", f"Wrong pin card {data['cardnumber']}")
+        messagebox.showwarning("Warning", 
+         f"Wrong pin for card {data['card number']}")
+
 config_btn = {
     'bg': 'pink',
     'fg': 'purple',
     'padx': 20,
     'pady': 15,
     'font': ('Times', 20),
+    'width': 16
 }
 config_entry = {
     'bg': 'pink',
@@ -36,8 +39,31 @@ root.config(bg='pink')
 e_pin = Entry(root, cnf=config_entry)
 btn_enter = Button(root, text='Enter', cnf=config_btn, command=enter)
 manage_window = Toplevel(root)
+change_pin_window = Toplevel(manage_window)
+transfer_window = Toplevel(manage_window)
+withdraw_window = Toplevel(manage_window)
 manage_window.protocol('WM_DELETE_WINDOW', root.destroy)
+change_pin_window.protocol('WM_DELETE_WINDOW', root.destroy)
+transfer_window.protocol('WM_DELETE_WINDOW', root.destroy)
+withdraw_window.protocol('WM_DELETE_WINDOW', root.destroy)
+manage_window.config(bg='pink')
+change_pin_window.config(bg='pink')
+transfer_window.config(bg='pink')
+withdraw_window.config(bg='pink')
 manage_window.withdraw()
+change_pin_window.withdraw()
+transfer_window.withdraw()
+withdraw_window.withdraw()
+btn_withdraw   = Button(manage_window, text='Withdraw', cnf=config_btn)
+btn_transfer   = Button(manage_window, text='Transfer', cnf=config_btn)
+btn_balance    = Button(manage_window, text='Balance', cnf=config_btn)
+btn_change_pin = Button(manage_window, text='Change Pin', cnf=config_btn)
+btn_withdraw.grid  (row=1, column=2, sticky='news', padx=20, pady=10)
+btn_transfer.grid  (row=1, column=1, sticky='news', padx=20, pady=10)
+btn_balance.grid   (row=2, column=1, sticky='news', padx=20, pady=10)
+btn_change_pin.grid(row=2, column=2, sticky='news', padx=20, pady=10)
+
+
 e_pin.pack(padx=15, pady=10)
 btn_enter.pack(padx=15, pady=10)
 mainloop()
